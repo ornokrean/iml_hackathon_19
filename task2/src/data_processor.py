15983c313c95203c0c3082503a34c42c8c7eb842
davidnir1 <david.nir1@mail.huji.ac.il>
2019-06-06 10:45:22 +0300

    ALLAHU AKBARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR

--- task2/src/data_processor.py
+++ task2/src/data_processor.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import sklearn as skl
+import seaborn
 
 
 CSV_PATH = "Crimes_since_2005.csv"
@@ -12,6 +13,7 @@ CSV_PATH = "Crimes_since_2005.csv"
 
 def read_file_into_matrix(path):
 	pd_df = pd.read_csv(path)
+
 	return pd_df
 
 
