d40d1b26f9c6e5ccfb03528c7a2fedb162904fd3
davidnir1 <david.nir1@mail.huji.ac.il>
2019-06-06 20:07:40 +0300

    FOR ASGAAAAAARRRRDDDDD

--- task2/src/train_data.py
+++ task2/src/train_data.py
@@ -1,10 +1,14 @@
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+
 from task2.src.data_processor import prepare_data
 import numpy as np
 from matplotlib import pyplot as plt
 
+from pandas import DataFrame
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 
@@ -21,8 +25,8 @@ def split_data(data, ratio):
 
 
 def get_success_rate(learner, test, test_labels):
-	prediction_fails = learner.predict(test) == test_labels
-	ltest = list(prediction_fails)
+	prediction_success_indices = learner.predict(test) == test_labels
+	ltest = list(prediction_success_indices)
 	return round(ltest.count(True) / float(len(ltest)), 3)
 
 
@@ -47,16 +51,49 @@ def get_tree_success_rate(data, ratio, iterations,depth = 6):
 	return mean/iterations
 
 
+def test_ada(data,split_ratio=0.3):
 
+	train, test, train_labels, test_labels = split_data(data, split_ratio)
+
+	ab = AdaBoostClassifier(
+		DecisionTreeClassifier(max_depth=8),
+		n_estimators=122,
+		learning_rate=1.1)
+
+	ab.fit(train, train_labels)
+	return get_success_rate(ab,test,test_labels)
+
+def test_KNN(data,split_ratio=0.3):
+	train, test, train_labels, test_labels = split_data(data, split_ratio)
+
+	knn=KNeighborsClassifier(n_neighbors = 8).fit(train, train_labels)
+
+	knn.fit(train, train_labels)
+	return get_success_rate(knn,test,test_labels)
+
+def test_random_forest(data,split_ratio=0.3):
+	train, test, train_labels, test_labels = split_data(data, split_ratio)
+
+	rf = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=42)
+
+	rf.fit(train, train_labels)
+
+	return get_success_rate(rf,test,test_labels)
 
 def main():
+
 	# get processed data
-	data = prepare_data(CSV_PATH, 15000)
+	data = prepare_data(CSV_PATH, 10000)
 
 
-
-	# get average error rate for a single tree
+	# todo this learner yields 0.3-0.4 success rate
+	print("ada success:",test_ada(data))
+	# todo this learner yields 0.3-0.4 success rate
+	print("knn success:",test_KNN(data))
+	# todo this learner yields 0.5-0.6 success rate
 	print("Single tree success rate: ",get_tree_success_rate(data,0.3,5))
+	# todo this learner yields 0.5-0.6 success rate
+	print("Random forest success rate: ",test_random_forest(data))
 
 
 
