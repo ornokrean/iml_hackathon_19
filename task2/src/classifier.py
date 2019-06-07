bc6e67e1e14bceefe4f941c8af452432909bff53
ornokrean <or.nokrean@mail.huji.ac.il>
2019-06-07 08:50:19 +0300

    qwertyuiop[

--- task2/src/classifier.py
+++ task2/src/classifier.py
@@ -2,14 +2,16 @@ import pickle
 import numpy as np
 CLASS_HEADER = 'Primary Type'
 from task2.src.data_processor import prepare_data, align_columns
-
+import pandas as pd
 
 def get_learner():
-	loaded_learner = pickle.load(open("learner_data.sav",'rb'))
+	# loaded_learner = pickle.load(open("learner_data.sav",'rb'))
+	loaded_learner = pd.read_hdf('learner_data.h5','d1')
 	return loaded_learner
 
 def get_headers():
-	loaded_headers = pickle.load(open("headers_data.sav",'rb'))
+	# loaded_headers = pickle.load(open("headers_data.sav",'rb'))
+	loaded_headers = pd.read_hdf('headers_data.h5','d1')
 	return loaded_headers
 
 def align_columns(known_headers, test_data):
